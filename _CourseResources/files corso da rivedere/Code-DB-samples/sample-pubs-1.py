600cd4546feb2d7db73302c3fee35d92254c2dc5
lorenzoFerri95 <61703355+lorenzoFerri95@users.noreply.github.com>
2020-10-07 14:12:23 +0200

    update_07_10_2020

---
+++ _CourseResources/files corso da rivedere/Code-DB-samples/sample-pubs-1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 14 22:44:23 2018
+
+@author: Anna
+"""
+
+import pyodbc
+
+#connect to data source, using userName and userPWD
+server = 'tcp:apa.di.unipi.it' 
+database = 'pubs' 
+username = 'lbi' 
+password = 'pisa' 
+connectionString = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password
+cnxn = pyodbc.connect(connectionString)
+cursor = cnxn.cursor()
+
+query="SELECT title, price FROM titles"
+cursor.execute(query)
+# scan results
+for row in cursor:
+    title = row.title
+    price = row.price
+    if price is not None:
+        print (title + ", " +str(price))
+        print(title + ", {0:.2f}".format(price))
+    else:
+        print(title)
+cursor.close()
+cnxn.close()
