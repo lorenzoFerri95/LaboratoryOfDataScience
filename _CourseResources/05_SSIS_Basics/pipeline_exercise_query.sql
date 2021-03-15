e5c0668d0c36e56e36205d473d39e7229e2c2415
lorenzoFerri95 <61703355+lorenzoFerri95@users.noreply.github.com>
2021-03-15 12:29:44 +0100

    spostamento_files_del_corso

--- _CourseResources/05_SSIS_Basics/pipeline_exercise_query.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT product_id, SUM(store_sales - store_cost)/SUM(unit_sales) AS avg_gain
-FROM sales_fact
-GROUP BY product_id
-ORDER BY avg_gain DESC
-
-
--- SELECT TOP(10) * FROM sales_fact ORDER BY product_id
